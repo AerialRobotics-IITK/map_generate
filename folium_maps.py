b608e6da8b23b8e1e9f653e2397f6a3dda8dd76e
KsKab <kabeer@iitk.ac.in>
2019-12-24 20:03:33 +0530

    Commented folium_maps.py

--- folium_maps.py
+++ folium_maps.py
@@ -2,18 +2,24 @@ import folium
 import os
 
 if __name__ == "__main__":
+    #Required variables. Please refer to the README to know what each variable does     
     center = [26.519608, 80.232266]
     zoom = 20
+
+    #Constructing folium map object
     map = folium.Map(location = center, zoom_start = zoom )
+
+    #Opening gps.txt and reading the coordinates. num_coords specifies the number of points to marks    
     f = open(os.path.expanduser('~') + '/.ros/router/gps.txt')
     text = f.read()
     list_words = text.split('\n')
     num_coords = (int)(list_words[0])
     
     for i in range (1,num_coords+1):
+        #Adding a pushpin marker for each point with the requisite latitude and longitude
         lat,lon = list_words[i].split(' ')
-        print(lat)
-        print(lon)
+        print(lat,lon,sep=' ')
         folium.Marker([lat, lon], popup = lat + ", " + lon).add_to(map) 
     
+    #Saving the map
     map.save(os.path.expanduser('~')+'/.ros/router/map.html')
